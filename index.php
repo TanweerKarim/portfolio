3e6a05fd839d5200748fc64baf671c53dd528203
TanweerKarim <tanweerkarim7@gmail.com>
2022-08-05 02:22:20 +0530

    Fifth commit

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 				<div class="col-md-8 col-md-offset-2 text-center">
 					<div class="display-t js-fullheight">
 						<div class="display-tc js-fullheight animate-box" data-animate-effect="fadeIn">
-							<div class="profile-thumb" sstyle="background-image: url(images/portfolio-1.png);"></div>
+							<div class="profile-thumb" style="background-image: url(images/tanweer.png);"></div>
 							<h1><span>Tanweer Karim</span></h1>
 							<p>
 								<ul class="fh5co-social-icons">
